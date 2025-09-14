5279a50751d036c0995b0cdc8f73ab7880649583
Giedrius Mirklys <giedriusmirklys8@gmail.com>
2025-09-14 15:12:18 +0000

    new data new analysis

--- analysis_scripts/shared/logging.py
+++
@@ -1,9 +0,0 @@
-import logging
-import logging.config
-
-import yaml
-
-logging.config.dictConfig(
-    yaml.load(open("./configs/logger_config.yaml", "r"), Loader=yaml.FullLoader)
-)
-logger = logging.getLogger("copydraw")
