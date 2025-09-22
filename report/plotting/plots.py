8b54b1c83f3e265c01a752ed759be8aa4cb393cd
Giedrius Mirklys <giedriusmirklys8@gmail.com>
2025-09-22 09:08:28 +0200

    Delete report/plotting directory

--- report/plotting/plots.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-
-def save_figure(filename, formats=[".png", ".svg"]):
-    for fmt in formats:
-        plt.savefig(f"{filename}.{fmt}", dpi=300)
-        print(f"Figure saved as {filename}.{fmt}")
