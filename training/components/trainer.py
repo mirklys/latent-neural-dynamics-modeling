092bdc0b00e914e6b5110b61ebcf4103301de0a5
Giedrius Mirklys <giedriusmirklys8@gmail.com>
2025-10-27 20:45:39 +0000

    somthing

---
+++ training/components/trainer.py
@@ -0,0 +1,8 @@
+from utils.config import Config
+
+class Trainer:
+
+    def __init__(self, config: Config):
+        pass
+    def train(self):
+        return NotImplemented
---
+++ training/train.py
@@ -0,0 +1,34 @@
+from utils.config import get_config
+from utils.logger import get_logger
+import argparse
+from training.components.trainer import Trainer
+
+def train(config):
+    logger = get_logger()
+    logger.info("Initializing training...")
+    logger.info(f"Training configuration:\n{config}")
+
+    trainer = Trainer(config)
+
+    logger.info("Training completed successfully!")
+
+
+def main(args):
+    config = get_config(args.config)
+    logger = get_logger()
+    logger.info(f"Configuration loaded from: {args.config}")
+    logger.info(f"Config content:\n{config}")
+
+    train(config)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Train model using specified configuration."
+    )
+    parser.add_argument(
+        "--config", type=str, required=True, help="Path to the configuration file."
+    )
+    args = parser.parse_args()
+
+    main(args)
